c761fd6e9c9e536aa1f275f9c685aa3c12f0f9db
Purnima <poornajyoti@gmail.com>
2018-07-02 11:55:36 +0530

    In range used

--- sundays.py
+++ sundays.py
@@ -1,11 +1,10 @@
 import calendar
 a=int(input("Enter the year: "))
 i=1
-#while i in range(1,13):
 k=0
-while i<13:
+while i in range(1,13):
 	j=1
-	while j<32:
+	while j in range (1,32):
 		try:
 			l=calendar.weekday(a,i,j)
 			if(l==6):
